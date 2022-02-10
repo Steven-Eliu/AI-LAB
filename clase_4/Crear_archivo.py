72531a5e8e2192bf85349b3a2d94db8bfcf282dd
Steven-Eliu <steven_eliu@live.com.mx>
2022-02-09 19:22:51 -0600

    comentarios

--- clase_4/Crear_archivo.py
+++ clase_4/Crear_archivo.py
@@ -9,6 +9,7 @@ while (ingresar == 'Y') :
     Altura = input('Ingrese altura en CM, (ejem: 170): ')
     Peso = input('Ingrese el peso en KG:')
     No_pie = input('Ingrese el No de calzado: ')
+    # Asegurar que no se ingresen datos vacios
     if ((len(Clase) > 0) and (len(Altura) > 0) and (len(Peso) > 0) and (len(No_pie) > 0)):
         x.append(Clase)
         x.append(Altura)
@@ -25,3 +26,5 @@ with archivo:
     escritor.writerows(datos)
 
 print('Escritura exitosa')
+
+# Se creo la base de datos de nuestro proximo algoritmo
